fe4a27610f8d474ec6070db67924731d07d5e507
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-12-16 20:02:09 +0300

    Fix generic tag compatibility

--- src/DocBlock/Tag/GenericTag.php
+++ src/DocBlock/Tag/GenericTag.php
@@ -8,4 +8,16 @@ namespace TypeLang\PhpDocParser\DocBlock\Tag;
  * This tag is created if the docblock is not supported by parser but looks
  * like a correct docblock.
  */
-final class GenericTag extends Tag implements InvalidTagInterface {}
+final class GenericTag extends Tag implements InvalidTagInterface
+{
+    private static ?\Throwable $reason = null;
+
+    public function getReason(): \Throwable
+    {
+        return self::$reason ??= new \OutOfRangeException(
+            message: \vsprintf('Tag "%s" is not supported by the parser', [
+                $this->getName(),
+            ]),
+        );
+    }
+}
