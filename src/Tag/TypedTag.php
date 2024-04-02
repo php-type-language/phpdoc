a65e221496a1e65b073341626e00837dce01b192
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-04-02 23:29:05 +0300

    Mark TypeStatement is optional in TypedTag

--- src/Tag/TypedTag.php
+++ src/Tag/TypedTag.php
@@ -8,6 +8,8 @@ use TypeLang\Parser\Node\Stmt\TypeStatement;
 
 /**
  * Requires a `type-lang/parser` dependency for {@see TypeStatement} support.
+ *
+ * @psalm-suppress UndefinedClass : Expects optional `type-lang/parser` dependency `type-lang/parser`.
  */
 abstract class TypedTag extends Tag
 {
